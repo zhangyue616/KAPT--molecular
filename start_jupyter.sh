4a056d23e4daafb6dce883337aaafd76c8752ebd
zhangyue616 <68990166+zhangyue616@users.noreply.github.com>
2026-02-10 07:12:55 +0800

    Delete start_jupyter.sh

--- start_jupyter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🚀 启动Jupyter Lab..."
-eval "$(conda shell.bash hook)"
-conda activate kano
-echo "🌐 访问地址: http://localhost:8888"
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root
